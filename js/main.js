344f817a0fc30919948dc25a51a74047bb1851d8
max1755 <112920950+max1755@users.noreply.github.com>
2022-12-05 21:34:20 -0500

    basic css and finished number guesser

--- js/main.js
+++ js/main.js
@@ -1,24 +1,51 @@
 document.querySelector(".guess").addEventListener("click", acceptGuess);
-let counter = 0;
-let randomValue = 0;
+
+const gameVariable = 
+{
+counter: 0,
+randomValue: 0,
+int: 1,
+};
 
 function acceptGuess() {
 
     let input = parseInt(document.querySelector('.input').value);
 
-    if (counter === 0)
-    {
-        randomValue = Math.floor(Math.random() * 10) * Math.floor(Math.random() * 10) + 1;
-        counter = 1;
+    if (gameVariable.int === 5){
+        alert("Out of tries");
+        location.reload();
     }
 
-    if (input > randomValue){
+    if (gameVariable.counter === 0)
+    {
+        gameVariable.randomValue = Math.floor(Math.random() * 5) * Math.floor(Math.random() * 5) + 1;
+        gameVariable.counter = 1;
+    }
+
+    if (input > gameVariable.randomValue){
     document.querySelector('.message').innerHTML = "My number isn't that big!";
+    coloring();
     }
-   if (input < randomValue){
+   if (input < gameVariable.randomValue){
     document.querySelector('.message').innerHTML = "My number is bigger than that!";
+    coloring();
     }
-   if (input === randomValue){
+   if (input === gameVariable.randomValue){
     document.querySelector('.message').innerHTML = "That's right! Congratulations!";
+    correct();
    }
+}
+
+function coloring()
+{
+    document.getElementById("cell"+gameVariable.int).style.backgroundColor = 'red';
+    gameVariable.int = gameVariable.int + 1;
+}
+
+function correct()
+{
+    document.getElementById("cell1").style.backgroundColor = 'green';
+    document.getElementById("cell2").style.backgroundColor = 'green';
+    document.getElementById("cell3").style.backgroundColor = 'green';
+    document.getElementById("cell4").style.backgroundColor = 'green';
 }
